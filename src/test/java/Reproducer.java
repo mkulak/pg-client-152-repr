cc55b9153c7f3f70f301691d621f0b5496739798
mkulak <kleshney@gmail.com>
2018-10-31 22:30:13 +0100

    beautify test code

--- src/test/java/Reproducer.java
+++ src/test/java/Reproducer.java
@@ -41,31 +41,33 @@ public class Reproducer {
                 .setMaxSize(1);
 
         PgPool client = PgClient.pool(options);
+        client.query("CREATE TABLE account(id INTEGER PRIMARY KEY)", ar -> {
+                    if (ar.failed()) {
+                        context.fail(ar.cause());
+                    } else {
+                        async.complete();
+                    }
+                }
+        );
+        async.awaitSuccess(10000);
+
+        Async async2 = context.async();
         client.getConnection(conAr -> {
             if (conAr.failed()) {
                 context.fail(conAr.cause());
             }
             PgConnection con = conAr.result();
             con.exceptionHandler(context::fail);
-            con.query("CREATE TABLE account(id INTEGER PRIMARY KEY)", ar -> {
-                System.out.println(ar.succeeded());
+//            List<Tuple> batch = Arrays.asList(Tuple.of(1)); //uncomment this line and comment next to make test pass
+            List<Tuple> batch = new ArrayList<>();
+            con.preparedBatch("INSERT INTO account VALUES ($1)", batch, ar -> {
                 if (ar.failed()) {
                     context.fail(ar.cause());
                 }
-//                List<Tuple> batch = Arrays.asList(Tuple.of(1)); //uncomment this line and comment next to make test pass
-                List<Tuple> batch = new ArrayList<>();
-                con.preparedBatch("INSERT INTO account VALUES ($1)", batch, ar1 -> {
-                    System.out.println(ar1.succeeded());
-                    if (ar1.failed()) {
-                        context.fail(ar1.cause());
-                    }
-                    System.out.println(ar1.result().rowCount());
-                    assertEquals(1, ar1.result().rowCount());
-                    async.complete();
-                });
+                assertEquals(1, ar.result().rowCount());
+                async2.complete();
             });
         });
-
-        async.awaitSuccess(20000);
+        async2.awaitSuccess(10000);
     }
 }
